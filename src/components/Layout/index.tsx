cc2ff6243b15c7b5c9b0f6c4132fa46827a16f9f
Choi-Jinwoo <chlwlsdn0828@gmail.com>
2021-10-12 16:30:57 +0900

    add: Layout에 Header 추가

--- src/components/Layout/index.tsx
+++ src/components/Layout/index.tsx
@@ -2,15 +2,18 @@ import React from 'react';
 import { ThemeProvider } from 'styled-components';
 import GlobalStyle from '../../styles/GlobalStyle';
 import { theme } from '../../styles/theme';
+import Header from '../Header';
 
 type Props = {
   children?: JSX.Element | JSX.Element[];
+  path: string;
 };
 
-const Layout = ({ children }: Props) => (
+const Layout = ({ children, path }: Props) => (
   <>
     <ThemeProvider theme={theme}>
       <GlobalStyle />
+      <Header path={path} />
       {children}
     </ThemeProvider>
   </>
