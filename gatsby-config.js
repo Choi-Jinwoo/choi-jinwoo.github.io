7517cb477711daa494a196d2bbd9449d29d682a9
Choi-Jinwoo <chlwlsdn0828@gmail.com>
2021-10-12 10:22:06 +0900

    chore: gatsby mdx 관련 plugin 설치

--- gatsby-config.js
+++ gatsby-config.js
@@ -15,10 +15,18 @@ module.exports = {
     {
       resolve: `gatsby-source-filesystem`,
       options: {
+        name: `blog`,
+        path: `${__dirname}/blog`,
+      },
+    },
+    {
+      resolve: `gatsby-source-filesystem`,
+      options: {
         name: `images`,
         path: `${__dirname}/src/images`,
       },
     },
+    `gatsby-plugin-mdx`,
     `gatsby-transformer-sharp`,
     `gatsby-plugin-sharp`,
     {
