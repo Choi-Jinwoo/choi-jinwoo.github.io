86333c2e30af6cde07c273e9154d91848135ed88
Choi-Jinwoo <chlwlsdn0828@gmail.com>
2021-10-12 22:11:14 +0900

    add: PostList 컴포넌트 grid view 로 개발

---
+++ src/components/Main/PostList/index.tsx
@@ -0,0 +1,23 @@
+import React from 'react';
+import styled from 'styled-components';
+import useAllPosts from '../../../hooks/useAllPosts';
+import PostItem from '../PostItem';
+
+const Container = styled.section`
+  margin: 0 auto;
+  width: ${props => props.theme.inner.desktop};
+  display: grid;
+  grid-template-columns: repeat(3, 1fr);
+  column-gap: 60px;
+  row-gap: 20px;
+`;
+
+const PostList = () => {
+  const posts = useAllPosts();
+
+  const postItems = posts.map(post => <PostItem key={post.id} post={post} />);
+
+  return <Container>{postItems}</Container>;
+};
+
+export default PostList;
