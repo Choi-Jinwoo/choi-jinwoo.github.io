6e67fb7c9df2a87398d1c59a8518cbd0c323fd76
Choi-Jinwoo <chlwlsdn0828@gmail.com>
2021-10-12 12:04:09 +0900

    add: theme gray color 추가 및 sizes 추가

--- src/styles/theme.ts
+++ src/styles/theme.ts
@@ -1,10 +1,28 @@
 const colors = {
   white: '#ffffff',
-  black: '#222222',
+  gray1: '#ebe9ec',
+  gray2: '#d9d9d9',
+  gray3: '#ababab',
+  gray4: '#848484',
+  gray5: '#747474',
+  gray6: '#5d5d5d',
+  gray7: '#444444',
+  black: '#1d1d1d',
+};
+
+const sizes = {
+  xxLarge: '2rem',
+  xLarge: '1.75rem',
+  large: '1.5rem',
+  medium: '1.25rem',
+  regular: '1rem',
+  small: '0.75rem',
+  tiny: '0.5rem',
 };
 
 export const theme = {
   colors,
+  sizes,
 };
 
 export type Theme = typeof theme;
