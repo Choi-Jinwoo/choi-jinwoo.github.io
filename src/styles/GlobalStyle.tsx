1c2db735af31c829133316785e163744b2ef5479
Choi-Jinwoo <chlwlsdn0828@gmail.com>
2021-10-12 22:08:21 +0900

    style: anchor tag visited 스타일 수정

--- src/styles/GlobalStyle.tsx
+++ src/styles/GlobalStyle.tsx
@@ -4,8 +4,17 @@ import reset from 'styled-reset';
 const GlobalStyle = createGlobalStyle`
   ${reset}
 
+  body {
+    line-height: normal;
+    color: ${props => props.theme.colors.black};
+  }
+
   a {
     text-decoration: none;
+
+    &:visited {
+      color: inherit;
+    }
   }
 `;
 
