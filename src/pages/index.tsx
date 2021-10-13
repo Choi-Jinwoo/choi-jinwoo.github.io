39fccbdef9cf172fd1d4f086c7ce2410a0e0b10e
Choi-Jinwoo <chlwlsdn0828@gmail.com>
2021-10-13 12:00:38 +0900

    add: IndexPage의 PostList 컴포넌트 랜더링

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,5 +1,13 @@
-import * as React from 'react';
+import React from 'react';
+import Layout from '../components/Layout';
+import PostList from '../components/Main/PostList';
 
-const IndexPage = () => <p>main</p>;
+const IndexPage = () => {
+  return (
+    <Layout path="/">
+      <PostList />
+    </Layout>
+  );
+};
 
 export default IndexPage;
