38c8aa89f1d6e31934ae2136e3a590418e5d0419
Choi-Jinwoo <chlwlsdn0828@gmail.com>
2021-10-16 15:33:15 +0900

    fix: meta의 og를 name에서 property로 변경

--- src/components/Layout/index.tsx
+++ src/components/Layout/index.tsx
@@ -36,11 +36,11 @@ const Layout = ({ children, path, title, description, image }: Props) => {
         <Helmet htmlAttributes={{ lang: 'ko' }}>
           <title>{title}</title>
           <meta name="description" content={description} />
-          <meta name="og:title" content={title} />
-          <meta name="og:description" content={description} />
-          <meta name="og:url" content={siteUrl} />
-          <meta name="og:image" content={image} />
-          <meta name="og:site_name" content={siteName} />
+          <meta property="og:title" content={title} />
+          <meta property="og:description" content={description} />
+          <meta property="og:url" content={siteUrl} />
+          <meta property="og:image" content={image} />
+          <meta property="og:site_name" content={siteName} />
           <meta
             name="google-site-verification"
             content="vEvU7Ajs0Xs3jma64V2Ctvokx-e0s2q1xkofjRTUBnQ"
