67600ec2568632732897732390631edb82d77d2f
Choi-Jinwoo <chlwlsdn0828@gmail.com>
2021-10-16 15:16:51 +0900

    add: Layout 컴포넌트의 og 태그 설정 및 imag props 전달

--- src/components/Layout/index.tsx
+++ src/components/Layout/index.tsx
@@ -4,6 +4,7 @@ import { Helmet } from 'react-helmet';
 import GlobalStyle from '../../styles/GlobalStyle';
 import { theme } from '../../styles/theme';
 import Header from '../Header';
+import useSiteMetaData from '../../hooks/useSiteMetaData';
 
 const ContentWrapper = styled.div`
   margin: 0 auto;
@@ -23,24 +24,34 @@ type Props = {
   path: string;
   title: string;
   description?: string;
+  image?: string;
 };
 
-const Layout = ({ children, path, title, description = '' }: Props) => (
-  <>
-    <ThemeProvider theme={theme}>
-      <Helmet htmlAttributes={{ lang: 'ko' }}>
-        <title>{title}</title>
-        <meta name="description" content={description} />
-        <meta
-          name="google-site-verification"
-          content="vEvU7Ajs0Xs3jma64V2Ctvokx-e0s2q1xkofjRTUBnQ"
-        />
-      </Helmet>
-      <GlobalStyle />
-      <Header path={path} />
-      <ContentWrapper>{children}</ContentWrapper>
-    </ThemeProvider>
-  </>
-);
+const Layout = ({ children, path, title, description, image }: Props) => {
+  const { title: siteName, siteUrl } = useSiteMetaData();
+
+  return (
+    <>
+      <ThemeProvider theme={theme}>
+        <Helmet htmlAttributes={{ lang: 'ko' }}>
+          <title>{title}</title>
+          <meta name="description" content={description} />
+          <meta name="og:title" content={title} />
+          <meta name="og:description" content={description} />
+          <meta name="og:url" content={siteUrl} />
+          <meta name="og:image" content={image} />
+          <meta name="og:site_name" content={siteName} />
+          <meta
+            name="google-site-verification"
+            content="vEvU7Ajs0Xs3jma64V2Ctvokx-e0s2q1xkofjRTUBnQ"
+          />
+        </Helmet>
+        <GlobalStyle />
+        <Header path={path} />
+        <ContentWrapper>{children}</ContentWrapper>
+      </ThemeProvider>
+    </>
+  );
+};
 
 export default Layout;
