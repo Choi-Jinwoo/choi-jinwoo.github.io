a5f21fc58ca6dde5455ba01bc258a3e7b298ec29
Choi-Jinwoo <chlwlsdn0828@gmail.com>
2021-10-17 12:07:51 +0900

    fix: utterances의 repo 주소 변경

--- src/hooks/useAppendUtterances.ts
+++ src/hooks/useAppendUtterances.ts
@@ -2,7 +2,7 @@ import { MutableRefObject, useEffect } from 'react';
 
 const utterancesSettings = {
   src: 'https://utteranc.es/client.js',
-  repo: 'Choi-Jinwoo/blog',
+  repo: 'Choi-Jinwoo/choi-jinwoo.github.io',
   'issue-term': 'pathname',
   label: 'utterances',
   theme: 'github-light',
