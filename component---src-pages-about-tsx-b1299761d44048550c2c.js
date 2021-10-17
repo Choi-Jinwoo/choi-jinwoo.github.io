facdff39737d3a5867cdb6a54cbcd04c8da92704
Choi-Jinwoo <chlwlsdn0828@gmail.com>
2021-10-17 11:54:46 +0900

    Updates

---
+++ component---src-pages-about-tsx-b1299761d44048550c2c.js
@@ -0,0 +1,2 @@
+"use strict";(self.webpackChunkgatsby_starter_default=self.webpackChunkgatsby_starter_default||[]).push([[49],{7483:function(e,t,a){a.r(t);var n=a(5444),u=a(7294);t.default=function(){return u.createElement("main",null,u.createElement("h1",null,"about"),u.createElement(n.Link,{to:"/"},"메인"))}}}]);
+//# sourceMappingURL=component---src-pages-about-tsx-b1299761d44048550c2c.js.map
