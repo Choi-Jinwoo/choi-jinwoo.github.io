f5a95a1d2b886d123e6474b8bc73278f2ee14b59
Choi-Jinwoo <chlwlsdn0828@gmail.com>
2021-11-09 09:18:41 +0900

    add: Twitter Open Graph 태그 추가

--- src/components/SEO/index.tsx
+++ src/components/SEO/index.tsx
@@ -19,11 +19,18 @@ const SEO = ({ title, description, image }: Props) => {
         {title} | {siteName}
       </title>
       <meta name="description" content={description} />
+      {/* Open Graph */}
       <meta property="og:title" content={title} />
-      <meta property="og:description" content={description} />
       <meta property="og:url" content={`${siteUrl}${pathname}`} />
-      <meta property="og:image" content={image} />
       <meta property="og:site_name" content={siteName} />
+      {description && <meta property="og:description" content={description} />}
+      {image && <meta property="og:image" content={image} />}
+
+      <meta name="twitter:card" content="summary" />
+      <meta name="twitter:title" content={title} />
+      {description && <meta name="twitter:description" content={title} />}
+      {image && <meta name="twitter:image" content={image} />}
+
       <meta
         name="google-site-verification"
         content="XuYZDwUE6RHYN-MpEJhfTnegOVDz8jolBYYFBZT1A1I"
