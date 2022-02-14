4b3d3f333c5282d8ad22032dc2d3f67470015296
Choi-Jinwoo <chlwlsdn0828@gmail.com>
2022-02-14 13:27:20 +0900

    chore(SEO): gastby-plugin-sitemap to gatsby-plugin-advanced-sitemap

--- gatsby-config.js
+++ gatsby-config.js
@@ -56,7 +56,7 @@ module.exports = {
         ],
       },
     },
-    `gatsby-plugin-sitemap`,
+    `gatsby-plugin-advanced-sitemap`,
     `gatsby-plugin-robots-txt`,
     {
       resolve: `gatsby-plugin-feed`,
