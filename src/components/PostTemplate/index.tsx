a2b6307abf661831d7818058a24cd1b6f7207580
Choi-Jinwoo <chlwlsdn0828@gmail.com>
2022-02-16 10:08:16 +0900

    add(post): thumbnail optionally in post list

--- src/components/PostTemplate/index.tsx
+++ src/components/PostTemplate/index.tsx
@@ -86,8 +86,10 @@ const PostPage = ({ data }: Props) => {
     <Tag key={index} size="medium" tag={tag} />
   ));
 
-  const composePostImagePath = () => {
+  const composePostImagePath = (): string | undefined => {
     let imageSrc = '';
+
+    if (thumbnail === undefined) return undefined;
 
     if (Array.isArray(thumbnail.childImageSharp.fluid)) {
       imageSrc = thumbnail.childImageSharp.fluid[0].src;
